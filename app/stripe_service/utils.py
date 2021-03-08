d200b2ef36a59556d0e0cd607024fa6ae82b002a
zlopez101 <zachlopez9@gmail.com>
2021-03-07 18:52:35 -0600

    stripe checkout complete, not sure of renewal

---
+++ app/stripe_service/utils.py
@@ -0,0 +1,51 @@
+from ..controllers import UserController
+
+
+def handleCheckoutSessionCompleted(event: dict) -> bool:
+    """handle the checkout.session.completed event received from stripe webhook
+    
+    The customer document in MongoDB should have status updated to active.
+    The userController.createTimer() method should be called 
+
+    :param event: JSON from stripe
+    :type event: dict   
+    :return: True if handled successfully
+    :rtype: bool
+    """
+    print("handling checkout.session.completed...")
+    try:
+        user = UserController.getFromId(event["data"]["object"]["client_reference_id"])
+        user.createTimer()
+        user.set(active=True)
+        return True
+    # client_reference_id was not properly set...
+    except KeyError as e:
+        return False
+    except Exception as e:
+        print(f"There was an error: \n{e}")
+
+
+def handleInvoicePaid(event: dict) -> bool:
+    """handle the invoice.paid event
+
+    The userController.createTimer() method should be called so that token timer is replaced with the new value
+    
+    :param event: JSON from stripe
+    :type event: dict
+    :return: True if handled successfully
+    :rtype: bool
+    """
+    print(f"{} is handling the event")
+    return True
+
+
+def handleInvoicePaymentFailed(event: dict) -> bool:
+    """handle the invoice.payment_failed event
+
+    :param event: JSON from stripe
+    :type event: dict
+    :return: True if handled successfully
+    :rtype: bool
+    """
+    print("handling invoice.payment_failed event...")
+    return True
